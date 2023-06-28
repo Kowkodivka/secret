711f4b6b18bfb82eaf3b01cf93e226ff3c68ced4
Kowkodivka <theeugenet@gmail.com>
2023-06-28 07:08:04 +0700

    resize

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 use std::path::Path;
 
 use clap::{arg, Command};
-use image::{DynamicImage, GenericImageView, ImageBuffer, Rgb};
+use image::{imageops::FilterType::Lanczos3, DynamicImage, GenericImageView, ImageBuffer, Rgb};
 
-fn hide_image(source_image: &DynamicImage, secret_image: &DynamicImage) -> DynamicImage {
+fn hide_image(
+    source_image: &DynamicImage,
+    secret_image: &DynamicImage,
+    resize: bool,
+    expand: bool,
+) -> DynamicImage {
     let (source_width, source_height) = source_image.dimensions();
     let (secret_width, secret_height) = secret_image.dimensions();
 
-    if source_width < secret_width || source_height < secret_height {
-        panic!("The size of the secret image exceeds the size of the original image");
-    }
+    let resized_secret_image = if resize {
+        secret_image.resize_exact(source_width, source_height, Lanczos3)
+    } else if expand {
+        expand_image(secret_image, source_width, source_height)
+    } else {
+        secret_image.clone()
+    };
 
     let source_buffer = source_image.to_rgb8();
-    let secret_buffer = secret_image.to_rgb8();
+    let secret_buffer = resized_secret_image.to_rgb8();
 
     let mut hidden_buffer = ImageBuffer::new(source_width, source_height);
 
@@ -33,37 +42,6 @@ fn hide_image(source_image: &DynamicImage, secret_image: &DynamicImage) -> Dynam
     }
 
     DynamicImage::ImageRgb8(hidden_buffer)
-}
-
-fn normalize_image(hidden_image: &DynamicImage) -> DynamicImage {
-    let hidden_buffer = hidden_image.to_rgb8();
-    let mut normalized_buffer = ImageBuffer::new(hidden_buffer.width(), hidden_buffer.height());
-
-    let mut min_value = 255u8;
-    let mut max_value = 0u8;
-
-    for (_, _, pixel) in hidden_buffer.enumerate_pixels() {
-        for i in 0..3 {
-            let value = pixel[i];
-            min_value = min_value.min(value);
-            max_value = max_value.max(value);
-        }
-    }
-
-    for (x, y, pixel) in hidden_buffer.enumerate_pixels() {
-        let mut normalized_pixel = Rgb([0u8; 3]);
-
-        for i in 0..3 {
-            let value = pixel[i];
-            let normalized_value =
-                ((value - min_value) as f32 / (max_value - min_value) as f32 * 255.0) as u8;
-            normalized_pixel[i] = normalized_value;
-        }
-
-        normalized_buffer.put_pixel(x, y, normalized_pixel);
-    }
-
-    DynamicImage::ImageRgb8(normalized_buffer)
 }
 
 fn decrypt_image(hidden_image: &DynamicImage) -> DynamicImage {
@@ -184,6 +162,59 @@ fn extract_text_from_image(image: &DynamicImage) -> String {
     extracted_text
 }
 
+fn normalize_image(hidden_image: &DynamicImage) -> DynamicImage {
+    let hidden_buffer = hidden_image.to_rgb8();
+    let mut normalized_buffer = ImageBuffer::new(hidden_buffer.width(), hidden_buffer.height());
+
+    let mut min_value = 255u8;
+    let mut max_value = 0u8;
+
+    for (_, _, pixel) in hidden_buffer.enumerate_pixels() {
+        for i in 0..3 {
+            let value = pixel[i];
+            min_value = min_value.min(value);
+            max_value = max_value.max(value);
+        }
+    }
+
+    for (x, y, pixel) in hidden_buffer.enumerate_pixels() {
+        let mut normalized_pixel = Rgb([0u8; 3]);
+
+        for i in 0..3 {
+            let value = pixel[i];
+            let normalized_value =
+                ((value - min_value) as f32 / (max_value - min_value) as f32 * 255.0) as u8;
+            normalized_pixel[i] = normalized_value;
+        }
+
+        normalized_buffer.put_pixel(x, y, normalized_pixel);
+    }
+
+    DynamicImage::ImageRgb8(normalized_buffer)
+}
+
+fn expand_image(
+    source_image: &DynamicImage,
+    target_width: u32,
+    target_height: u32,
+) -> DynamicImage {
+    let (source_width, source_height) = source_image.dimensions();
+
+    let source_buffer = source_image.to_rgb8();
+    let mut expanded_buffer = ImageBuffer::new(target_width, target_height);
+
+    for (x, y, pixel) in expanded_buffer.enumerate_pixels_mut() {
+        if x < source_width && y < source_height {
+            let source_pixel = source_buffer.get_pixel(x, y);
+            *pixel = *source_pixel;
+        } else {
+            *pixel = Rgb([0, 0, 0]);
+        }
+    }
+
+    DynamicImage::ImageRgb8(expanded_buffer)
+}
+
 fn main() {
     let matches = Command::new("secret")
         .version("1.0")
@@ -196,6 +227,8 @@ fn main() {
                 .arg(arg!(--source <SOURCE>))
                 .arg(arg!(--secret <SECRET>))
                 .arg(arg!(--output <OUTPUT>))
+                .arg(arg!(--resize "Resizes the image"))
+                .arg(arg!(--expand "Expands the image"))
                 .arg_required_else_help(true),
         )
         .subcommand(
@@ -227,13 +260,16 @@ fn main() {
             let secret = sub_matches.get_one::<String>("secret").unwrap();
             let output = sub_matches.get_one::<String>("output").unwrap();
 
+            let resize = sub_matches.get_flag("resize");
+            let expand = sub_matches.get_flag("expand");
+
             let source_image =
                 image::open(&Path::new(&source)).expect("Failed to open source image");
             let secret_image =
                 image::open(&Path::new(&secret)).expect("Failed to open secret image");
 
             let normalized_image = normalize_image(&source_image);
-            let hidden_image = hide_image(&normalized_image, &secret_image);
+            let hidden_image = hide_image(&normalized_image, &secret_image, resize, expand);
 
             hidden_image
                 .save(&Path::new(&output))
